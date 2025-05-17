f15378145c2420ef7871bda76430fb02643a7ba6
Alejandra Duque <ing.frontendduque@gmail.com>
2025-05-17 00:23:07 -0500

    DQE-09-Slider-tecnologias

--- src/scripts/scrollReveal.js
+++ src/scripts/scrollReveal.js
@@ -1,14 +1,9 @@
-import ScrollReveal from 'scrollreveal';
-
-// Inicializar ScrollReveal
-const sr = ScrollReveal();
-
-// Aplicar animación a elementos con la clase 'reveal-text'
-sr.reveal('.reveal-text', {
-  origin: 'bottom',     // Dirección desde la que aparece el elemento
-  distance: '50px',     // Distancia que recorre el elemento durante la animación
-  duration: 1000,       // Duración de la animación en milisegundos
-  delay: 200,           // Retardo antes de que comience la animación
-  easing: 'ease-in-out',// Función de aceleración de la animación
-  reset: false          // Si es true, la animación se repetirá cada vez que el elemento entre en la vista
-});
+ ScrollReveal().reveal('.reveal-text', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 1000,
+    delay: 200,
+    opacity: 0,
+    easing: 'ease-in-out',
+    reset: false
+  });
